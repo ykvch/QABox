3fdd268698d0204d07b5ea1e46062dac39a13f27
yan123 <yankevych@gmail.com>
2015-02-14 12:14:26 +0200

    Update metatest.py

--- metatest.py
+++ metatest.py
@@ -4,12 +4,14 @@ from unittest import TestCase
 import itertools
 
 '''
-Idea:
+Rationale:
+
 Imagine we need to run the same test steps multiple times, but each time with different
 parameter combinatins.
-For 2 params we can represent this as a table, where columns stand for A values, and rows for B.
-So in case we need to test all combinations of possible values of A=(0,1,2) and B=['a','b','c'],
-this would require 9 tests as follows:
+For 2 params A and B, we can represent this as a table, where columns stand for A values,
+and rows for B values.
+So, if we need to test all possible combinations of A=(0,1,2) and B=['a','b','c'],
+that would require 9 tests as follows:
 +-------------------------------+
 |       |  A=0  |  A=1  |  A=2  |
 +-------------------------------+
@@ -23,7 +25,7 @@ this would require 9 tests as follows:
 Copy-pasting, or using nose generator plugin might sometimes not be an option.
 
 So here's the solution:
-1) Import MetaTest from current file.
+1) Import MetaTest from this file.
 2) Create a test class with `__metaclass__ = MetaTest`
 3) Inside it create test case methods that accept extra params for A and B
 and decorate them as follows:
@@ -31,12 +33,12 @@ and decorate them as follows:
 @with_combined(A=[0,1,2], B='abc')
 def test_method(self, A, B):...
 
-Note that extra params after `self` have to conform the one's in decorator.
-Note2 these do NOT HAVE to be named params, just make sure their amount and naming fit
-to be passed as test_method arguments
-Note3 there can be multiple such decorated test methods in each test class.
+Note: that extra params after `self` have to conform the one's in decorator.
+Note2: these do NOT HAVE to be only the named params, just make sure that their
+amount and naming fit test_method arguments.
+Note3: there can be multiple decorated test methods in each test class.
 
-That's all. Our metaclass will spawn extra 9 methods each containing
+That's all. Our metaclass will then spawn extra 9 methods each containing
 a `test_method` call, but with different param combinations (as shown below):
 test_method(A=0, B='a'); test_method(A=0, B='b'); test_method(A=0, B='c');
 test_method(A=1, B='a'); test_method(A=1, B='b'); ... test_method(A=2, B='c');
@@ -46,37 +48,39 @@ OR with nose tests runner.
 '''
 
 def mix_params(args_kwargs):
-    '''Helper generator function. Takes tuple of named and unnamed params
+    '''Helper generator function. Takes args, kwargs as single tuple
     and returns all their combinations. Each item inside args or kwargs is
-    expected to be an iterable of possible input values'''
+    expected to be an iterable of all desired input values for certain variable'''
     args, kwargs = args_kwargs
     args_len = len(args)
     for i in itertools.product(*itertools.chain(args, kwargs.values())):
         yield tuple(i[:args_len]), dict(zip(kwargs.keys(), i[args_len:]))
 
 def with_combined(*args, **kwargs):
-    '''Decorator. Adds metatest_params=(args, kwargs) variable to decorated function
-    This field is required by MetaTest class to see which functions have to be turned
-    into multiple tests'''
+    '''Decorator. Pins metatest_params=(args, kwargs) variable on decorated
+    function or method. metatest_params is a marker for MetaTest class to see which
+    functions have to be turned into multiple tests'''
     def hook_args_kwargs(method):
         method.metatest_params = (args, kwargs)
         return method
     return hook_args_kwargs
 
 class MetaTest(type):
-    '''Generates multiple tests based on decorated method(s) in subtyped class'''
+    '''Generates multiple tests based on decorated method(s) in subtyped class.
+    Adds test_ prefix to each method, so it can be recognized as a test'''
     def __new__(cls, name, bases, attrs):
         for method in attrs.values():
             if callable(method) and hasattr(method, 'metatest_params'):
                 for test_args, test_kwargs in mix_params(method.metatest_params):
-                    print test_args, test_kwargs # Closure here!!!!
-                    def actual_test(self, ar=test_args, kw=test_args):
+                    # Closure here, using default args trick!!!
+                    def actual_test(self, ar=test_args, kw=test_kwargs):
                         return method(self, *ar, **kw)
-                    name = ('test_case ' + ', '.join(ar) + (', 'if ar else'') +
-                            ', '.join(str(k)+'='+str(v) for k,v in kw.items()))
+                    name = ('test_'+ method.__name__ + ' ' +
+                            ''.join(str(a)+', ' for a in test_args) +
+                            ', '.join(str(k)+'='+str(v) for k,v in test_kwargs.items()))
                     actual_test.__name__ = name
                     attrs[name] = actual_test
-        print (cls, name, bases, attrs)
+        # print (cls, name, bases, attrs)
         return super(MetaTest, cls).__new__(cls, name, bases, attrs)
 
 # Usage example:
@@ -85,7 +89,10 @@ class SuiteExample(TestCase):
     # runTest = lambda *args: True # for debugging purposes only
 
     def setUp(self):
-        print 'running setup'
+        print '\nrunning setup'
+
+    def tearDown(self):
+        print '\ndoing cleanup after test'
     
     # this one is decorated, so we'll spawn many tests from it
     # this will produce 18 tests, each containing a method call
@@ -93,7 +100,8 @@ class SuiteExample(TestCase):
     # or like (the 18th test): steps2execute(self, 3, col='c', extra='-')
     @with_combined((1,2,3), col=['a','b','c'], extra='+-')
     def steps2execute(self, row, col, extra):
-        print('executing steps with row='+str(row)+', col='+col', extra='+extra)
+        '''test steps'''
+        print('doing some steps with: row='+str(row)+', col='+col+', extra='+extra)
         self.assertFalse(row*col+extra)
 
 # Just a plain test suite without generations:
