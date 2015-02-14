ef8cb3b2f464bc9219c33eb7a9bc180ff0b653cd
yan123 <yankevych@gmail.com>
2015-02-14 03:42:39 +0200

    Update metatest.py

--- metatest.py
+++ metatest.py
@@ -1,24 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from unittest import TestCase
 import itertools
-import sys
+
+'''
+Idea:
+Imagine we need to run the same test steps multiple times, but each time with different
+parameter combinatins.
+For 2 params we can represent this as a table, where columns stand for A values, and rows for B.
+So in case we need to test all combinations of A,B, this would require 9 tests as follows:
++-------------------------------+
+|       |  A=0  |  A=1  |  A=2  |
++-------------------------------+
+| B='a' | test0 | test1 | test2 |
++-------------------------------+
+| B='b' | test3 | test4 | test5 |
++-------------------------------+
+| B='c' | test6 | test7 | test8 |
++-------------------------------+
+
+Copy-pasting, or using nose generator plugin are sometimes not an option.
+So here's the solution. Import MetaTest from current file.
+Then create a test class with `__metaclass__ = MetaTest`
+Inside it create test case methods that accept extra params for A and B
+and decorate them as follows:
+
+@with_combined(A=[0,1,2], B='abc')
+def test_method(self, A, B):...
+
+Note that extra params after `self` have to conform the one's in decorator.
+Note2 these do NOT HAVE to be named params, just make sure their amount and naming fit
+to be passed as test_method arguments
+
+That's all. Our metaclass will spawn extra 9 methods each containing
+a `test_method` call, but with different param combinations (as shown below):
+test_method(A=0, B='a'); test_method(A=0, B='b'); test_method(A=0, B='c');
+test_method(A=1, B='a'); test_method(A=1, B='b'); ... test_method(A=2, B='c');
+'''
 
 def mix_params(args_kwargs):
+    '''Helper generator function. Takes tuple of named and unnamed params
+    and returns all their combinations. Each item inside args or kwargs is
+    expected to be an iterable of possible input values'''
     args, kwargs = args_kwargs
     args_len = len(args)
     for i in itertools.product(*itertools.chain(args, kwargs.values())):
         yield tuple(i[:args_len]), dict(zip(kwargs.keys(), i[args_len:]))
 
-def with_combinations(*args, **kwargs):
+def with_combined(*args, **kwargs):
+    '''Decorator. Adds metatest_params=(args, kwargs) variable to decorated function
+    This field is required by MetaTest class to see which functions have to be turned
+    into multiple tests'''
     def hook_args_kwargs(method):
         method.metatest_params = (args, kwargs)
         return method
     return hook_args_kwargs
 
 class MetaTest(type):
-    '''Generates multiple tests upon decorated method in class'''
+    '''Generates multiple tests based on decorated method(s) in subtyped class'''
     def __new__(cls, name, bases, attrs):
         for method in attrs.values():
             if callable(method) and hasattr(method, 'metatest_params'):
@@ -33,21 +72,25 @@ class MetaTest(type):
         print (cls, name, bases, attrs)
         return super(MetaTest, cls).__new__(cls, name, bases, attrs)
 
-# Below goes usage example:
+# sage example:
 
-class SuiteOne(TestCase):
+class SuiteExample(TestCase):
     __metaclass__ = MetaTest # forces use of test generator
+    # runTest = lambda *args: True # for debugging purposes only
 
     def setUp(self):
         print 'running setup'
     
-    # this one is decorated, so we'll make many tests from it
-    @with_combinations((1,2,3), col=['a','b','c'])
-    def one(self, row, col):
-        self.assertFalse(row)
+    # this one is decorated, so we'll spawn many tests from it
+    # this will produce 18 tests, each containing a method call
+    # like (the 1st test): steps2execute(self, 1, col='a', extra='+')
+    # or like (the 18th test): steps2execute(self, 3, col='c', extra='-')
+    @with_combined((1,2,3), col=['a','b','c'], extra='+-')
+    def steps2execute(self, row, col, extra):
+        print('executing steps with row='+str(row)+', col='+col', extra='+extra)
+        self.assertFalse(row*col+extra)
 
-    runTest = lambda *args: True
-
+# Just a plain test suite without generations:
 class T2(TestCase):
     def test_case_two(self):
         assert True
