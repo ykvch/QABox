1f28bd21d8a61cf742d71eb52a30d7d9e7b1c36a
yan123 <yankevych@gmail.com>
2015-05-04 01:13:45 +0300

    initial server implementation

--- prettyrpc.py
+++ prettyrpc.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from xmlrpclib import ServerProxy
+from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 class PrettyProxy(object):
     def __init__(self, *args, **kwargs):
@@ -9,3 +10,11 @@ class PrettyProxy(object):
 
     def __getattr__(self, name):
         return lambda *args, **kwargs: getattr(self._real_proxy, name)(args, kwargs)
+
+class PrettyServer(SimpleXMLRPCServer):
+    def _dispatch(self, method, params):
+        '''Changes logic of parent _dispatch method to handle params as
+        a tuple of (args, kwargs)'''
+        # TODO: missing methods handling as in parent class
+        func = self.funcs[method]
+        return func(*params[0], **params[1])
