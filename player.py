0b91cea732c08d4369d947f9c259b9821cf28628
yan123 <yankevych@gmail.com>
2016-12-23 11:00:52 +0200

    IterBuffer added

--- player.py
+++ player.py
@@ -74,3 +74,36 @@ def detect_readmethod(uri):
     if uri.startswith('http'):
         return requests.get
     raise RuntimeError('Unknown read method for URI: {0}'.format(uri))
+
+class IterBuffer(object):
+    retry = 5
+    low = 1
+
+    def __init__(self):
+        self.buffer = deque()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        '''Buffered read as generator'''
+        print 'data.next...'
+        retry = self.retry
+        while (retry > 0) and self.is_low():
+            print 'retry ...', retry
+            retry -= 1
+            self.update_buffer()
+
+        return self.read_buffer()
+
+    __next__ = next # Py3
+
+    def is_low(self):
+        return len(self.buffer) < self.low
+
+    def update_buffer(self):
+        self.buffer.append(None)
+
+    def read_buffer(self):
+        return self.buffer.popleft()
+
