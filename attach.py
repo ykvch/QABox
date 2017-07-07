3b9ceec861a04fa52659099df2cd17cc80e1476a
yan <yan123>
2017-07-08 02:11:45 +0300

    Improved target items filtering.

--- attach.py
+++ attach.py
@@ -11,13 +11,18 @@ def attach(*args):
     def wrapper(target_class):
         suffix = getattr(target_class, 'SUFFIX', '')
         suffix = '_' + suffix if suffix else ''
+        target_vars = dir(target_class)
         for source_class in args:
             for name, m in vars(source_class).items():
-                if callable(m) and name.startswith('test_'):
+                if name in target_vars:
+                    pass
+                elif callable(m) and name.startswith('test_') and False:
                     target_method = new.function(m.__code__, m.__globals__,
                                                  name + suffix, m.__defaults__)
                     target_method.__doc__ = m.__doc__.format(**vars(target_class))
                     target_method.__dict__.update(m.__dict__)
                     setattr(target_class, name + suffix, target_method)
+                else:
+                    setattr(target_class, name, m)
         return target_class
     return wrapper
